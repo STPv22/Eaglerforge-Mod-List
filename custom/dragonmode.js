144969a0bf3797d5e722a7246f3c6755db7adea6
STPv22 <145866689+STPv22@users.noreply.github.com>
2024-01-30 15:24:46 -0800

    Update dragonmode.js

--- custom/dragonmode.js
+++ custom/dragonmode.js
@@ -3,9 +3,19 @@ PluginAPI.require("player");
 var isToggled = false;
 
 window.addEventListener("keydown", (event) => { //look for key down
-  if (event.key.toLowerCase()==="z"){
-    PluginAPI.player.capabilities.disableDamage = isToggled;
+  if (event.key.toLowerCase()==="x"){
+   isToggled = !isToggled;
+  }
+  if (isToggled === true){
+     PluginAPI.player.capabilities.disableDamage = isToggled;
+    PluginAPI.player.capabilities.allowFlying = isToggled;
+    PluginAPI.player.isImmuneToFire = isToggled;
+    PluginAPI.player.isImmuneToExplosions = isToggled;
+    PluginAPI.player.isInvisible = isToggled;
     PluginAPI.player.reload();
-    PluginAPI.displayToChat({msg: "&4&ldragonmode togglled:" +" "+ isToggled});
+    PluginAPI.displayToChat({msg: "&4&ldragonmode enabled"});
+  }
+  else if(!isToggled){
+    PluginAPI.displayToChat({msg: "&4&ldragonmode diabled"});
   }
 });
