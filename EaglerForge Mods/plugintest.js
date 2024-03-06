c95a321745acba597c7ea0d94f1b0a2977539fdc
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-03-06 15:19:56 -0800

    Update plugintest.js

--- EaglerForge Mods/plugintest.js
+++ EaglerForge Mods/plugintest.js
@@ -4,13 +4,6 @@ var isToggled = false;
 
 ModAPI.addEventListener("keydown", (event) => {
   if (event.key.toLowerCase() === "t"){
-      if (isToggled){
-          ModAPI.player.motionY += 1;
-          ModAPI.player.reload();
-      }
-      if (!isToggled){
-        isToggled = true;
-      }
-    ModAPI.displayToChat({msg: 'toggled'})
+      ModAPI.currentItem = 1;
   }
 }); 
