c1c77ecfd7758f381f651dca19414450f58fe5c2
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-03-07 11:54:59 -0800

    Update plugintest.js

--- EaglerForge Mods/plugintest.js
+++ EaglerForge Mods/plugintest.js
@@ -1,8 +1,9 @@
-ModAPI.require("player")
+//key = H
 var isToggled = false;
 
 ModAPI.addEventListener("key", (event) => {
   if (event.key == 35){
-      ModAPI.player.inventory.currentItem = 1
+    ModAPI.drawStringWithShadow({msg: "MEN", 2, 5, 0xAF3A20})
+    ModAPI.update()
   }
 }); 
