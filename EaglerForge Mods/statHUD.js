68a37b7fd0012a3f4867ae69e29f67b83d374f97
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-03-08 11:34:11 -0800

    Update statHUD.js

--- EaglerForge Mods/statHUD.js
+++ EaglerForge Mods/statHUD.js
@@ -50,7 +50,7 @@ function initializeHud() {
         statDisplay.style.left = "10px";
         statDisplay.style.background = "rgba(0, 0, 0, 0.7)";
         statDisplay.style.zIndex = "1000";
-        statDisplay.style.boxShadow = "0 0 5px #00ffee";
+        statDisplay.style.boxShadow = "0 0 30px #00ffee";
         statDisplay.style.borderRadius = "20px";
         statDisplay.style.backdropFilter = "blur(3px)";
         
