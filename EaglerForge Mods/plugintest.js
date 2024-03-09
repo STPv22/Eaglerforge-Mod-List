00561e6050397e2486a067ff7a0601a99386a6a3
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-03-08 18:45:53 -0800

    Update plugintest.js

--- EaglerForge Mods/plugintest.js
+++ EaglerForge Mods/plugintest.js
@@ -1,13 +1,4 @@
-//key = H
-var isToggled = false;
-ModAPI.require("player");
-
-ModAPI.addEventListener("key", (event) => {
-  if (event.key == 35){
-    ModAPI.displayToChat({msg: "h"})
-    
-    ModAPI.clickMouse()
-    ModAPI.player.motionY += 1;
-ModAPI.player.reload();
-  }
-}); 
+ModAPI.require("player"); 
+ModAPI.addEventListener("update", () => { //Every client tick
+  ModAPI.player.experienceLevel = 255;
+});
