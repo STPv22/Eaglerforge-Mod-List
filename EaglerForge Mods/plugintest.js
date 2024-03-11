8dcce0322e087945d0abd878166ea4dda543bf80
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-03-11 12:56:00 -0700

    Update plugintest.js

--- EaglerForge Mods/plugintest.js
+++ EaglerForge Mods/plugintest.js
@@ -1,7 +1,15 @@
-var isToggled = false;
-
-ModAPI.addEventListener("key", (event) => {
-  if (event.key == 35){
-      ModAPI.InventoryPlayerData.currentItem = 1;
+var gui = {
+  enabled: true,
+  draw: function(){
+    var menu = document.createElement("div");
+    menu.style = "background-color: black;";
   }
-}); 
+  jetpack:{
+    enabled: false;
+  }
+}
+
+//test
+ModAPI.addEventListener("drawhud", () => {
+    ModAPI.drawStringWithShadow({msg: "your text", x: 0, y: 0, color: -1}) // -1 is white
+})
