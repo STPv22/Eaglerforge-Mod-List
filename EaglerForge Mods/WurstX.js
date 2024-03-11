3c22ac76e5a219f83a96c0bf28a2761b7bcc000a
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-03-11 15:54:40 -0700

    test

---
+++ EaglerForge Mods/WurstX.js
@@ -0,0 +1,48 @@
+// §
+/*
+BY STPv22
+
+DESC:wip
+*/
+//requre
+ModAPI.require("player");
+
+//credits go to AstralisLLC for the code
+let playerFound = false; //is the player in a game?
+
+
+
+//object for holding all of the hax's GUI
+var gui = {
+  playerFound = false,
+  enabled: true,
+  draw: function(){
+      var menu = document.createElement("div");
+      menu.style = "background-color: black;";
+  },
+  jetpack:{
+    enabled: false;
+  }
+}
+
+//credits go to AstralisLLC for the code
+ //is the player in a game?
+function checkForGame() {
+    if (typeof ModAPI.player !== 'undefined') {
+        playerFound = true;
+    } else {
+        console.log('Player doesn\'t exist yet, please join a server or singleplayer world...');
+    }
+}
+
+checkForGame();
+
+if (gui.playerFound) ModAPI.displayToChat({msg: "§5WurstX by §6stpv22});
+
+ModAPI.addEventListener("key", (event) => {
+    if (gui.playerFound){
+      if(event.key == 42){ //left shift
+        gui.draw();
+      }
+    }
+})
