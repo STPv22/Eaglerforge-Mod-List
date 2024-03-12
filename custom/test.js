a47a73e9dfb6450fbf8367e8e954d01fbd43e284
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-03-12 16:37:06 +0000

    update

--- custom/test.js
+++
@@ -1,8 +0,0 @@
-var isToggled = false;
-
-window.addEventListener("keydown", (event) => {
-  if (event.key.toLowerCase()==="z"){
-    isToggled = !isToggled;
-    PluginAPI.displayToChat({msg: "Working!" + isToggled});
-  }
-});
