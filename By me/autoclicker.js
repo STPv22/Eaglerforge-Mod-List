b60ff775d66a2fa5ec8d7164a378244d773195e7
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-03-12 11:43:09 -0700

    Update autoclicker.js

--- By me/autoclicker.js
+++ By me/autoclicker.js
@@ -1,16 +1,19 @@
+// §
 var enabled = false;
 
 ModAPI.addEventListener("key", (event) => {
     if (event.key == 33) {//f
-      if (enabled) {
+      if (enabled == true) {
         enabled = false;
+          ModAPI.displayToChat({msg: "§5AutoClicker enabled!"})
       }
       if(!enabled){
         enabled = true;
+          ModAPI.displayToChat({msg: "§5AutoClicker disabled!"})
       }
     }
 })
 
 ModAPI.addEventListener("update", () => {//every time the client updates
-  if (enabled) ModAPI.clickMouse(); //left click. rightClickMouse()
+  if (enabled == true) ModAPI.clickMouse(); //left click. rightClickMouse()
 })
