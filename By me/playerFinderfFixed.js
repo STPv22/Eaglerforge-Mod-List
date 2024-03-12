34f43bede0a5db651dd176eff759b6a4b662cc9d
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-03-12 12:42:58 -0700

    Update playerFinderfFixed.js

--- By me/playerFinderfFixed.js
+++ By me/playerFinderfFixed.js
@@ -8,10 +8,6 @@ function drawLineToPlayer(player) {
     var centerY = ModAPI.getdisplayHeight() / 2;
     
     var playerPos = new Vec3(ModAPI.otherPlayerMPX, ModAPI.otherPlayerMPY, ModAPI.otherPlayerMPZ);
-    var screenPos = renderManager.getRenderViewEntity().getLook(1.0).normalize().subtract(playerPos).normalize();
-    
-    // Adjust line length to be within render distance
-    screenPos = screenPos.scale(ModAPI.mcinstance.gameSettings.renderDistanceChunks * 16);
     
     // Draw line
     renderManager.drawLine(centerX, centerY, centerX + screenPos.xCoord, centerY + screenPos.yCoord, 2, 0x00FF00);
